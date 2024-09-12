30edcd4c5489cbce3de8a81b36b4fbc19f4827af
Aiden Cullo <aidencullo@gmail.com>
2024-09-12 05:39:30 -1000

    Create the following validation rules on the grades collection:
    Each document must have a class_id field, which must be an integer between 0 and 300, inclusive.
    Each document must have a learner_id field, which must be an integer greater than or equal to 0.

--- db/conn.mjs
+++ db/conn.mjs
@@ -18,4 +18,35 @@ await db.collection("grades").createIndex({ class_id: 1 });
 await db.collection("grades").createIndex({ learner_id: 1 });
 await db.collection("grades").createIndex({ learner_id: 1, class_id: 1 });
 
+(async () => {
+  await db.command({
+    collMod: "grades",
+    // Pass the validator object
+    validator: {
+      // Use the $jsonSchema operator
+      $jsonSchema: {
+        bsonType: "object",
+        title: "Learner Validation",
+        // List required fields
+        required: ["class_id", "learner_id"],
+        // Properties object contains document fields
+        properties: {
+          class_id: {
+            bsonType: "int",
+            minimum: 0,
+	    maximum: 300,
+          },
+	  learner_id: {
+	    bsonType: "int",
+	    minimum: 0,
+	  },
+        },
+      },
+    },
+  });
+})();
+
+// fails verification
+// db.collection("grades").insertOne({ class_id: 1 });
+
 export default db;
